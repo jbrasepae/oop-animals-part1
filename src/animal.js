5c476325cc7e6600d9239feb15acc1e23413a051
Jeanette <jbrasepae@gmail.com>
2021-01-22 10:26:30 +0200

    added more tests

--- src/animal.js
+++ src/animal.js
@@ -2,16 +2,15 @@ class Animal {
 	constructor(name) {
 		this.name = name;
 	}
-
 	eat() {
 		return 'Food';
 	}
 }
+
 class Dog extends Animal {
 	constructor(name) {
 		super(name);
 	}
-
 	sounds() {
 		return 'Bark';
 	}
@@ -21,7 +20,6 @@ class Cat extends Animal {
 	constructor(name) {
 		super(name);
 	}
-
 	sounds() {
 		return 'Meow';
 	}
@@ -31,11 +29,9 @@ class Home {
 	constructor() {
 		this.adoptedAnimals = [];
 	}
-
 	adoptPet(pet) {
 		this.adoptedAnimals.push(pet);
 	}
-
 	makeAllSounds() {
 		for (var i = 0; i < this.adoptedAnimals.length; i++) {
 			this.adoptedAnimals[i].sounds();
@@ -43,18 +39,3 @@ class Home {
 	}
 }
 module.exports = { Dog, Cat, Home };
-// var home = new Home();
-// var dog1 = new Dog();
-// var dog2 = new Dog();
-// var cat = new Cat();
-
-// home.makeAllSounds();
-
-// home.adoptPet(dog1);
-// home.makeAllSounds();
-
-// home.adoptPet(cat);
-// home.makeAllSounds();
-
-// home.adoptPet(dog2);
-// home.makeAllSounds();
